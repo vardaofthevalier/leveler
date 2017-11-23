236e128d21cbc67cc2426b3878962330b560c0bd
Abigail Hahn <abbyhahn@level11.com>
2017-11-22 20:29:41 -0800

    More refactoring

--- pipelines/factory.go
+++ pipelines/factory.go
@@ -6,28 +6,38 @@ import (
 	"leveler/config"
 )
 
-func NewBasicPipeline(serverConfig *config.ServerConfig, pipelineConfig *BasicPipeline) (*Pipeline, error) {
-	// TODO: validate that integration configurations can be found for the user who submitted this pipeline
-	// if not, return an error to the caller
-
+func createJobsMap(serverConfig *config.ServerConfig, pipelineConfig *BasicPipeline) (map[string]PipelineJob, *Pipeline, error) {
 	var p = &Pipeline{}
 	var allJobs = make(map[string]PipelineJob)
 
 	// process jobs into a map for O(1) lookup later on, and also to verify that no duplicate names are found
 	for _, s := range pipelineConfig.Steps {
 		if _, ok := allJobs[s.Name]; ok {
-			return p, errors.New("Duplicate job names found in pipeline!")
+			return allJobs, p, errors.New("Duplicate job names found in pipeline!")
 		} else {
 			switch serverConfig.Platform.Name {
 			case "kubernetes":
 				j, err := NewKubernetesPipelineJob(serverConfig, s)
 				if err != nil {
-					return p, err
+					return allJobs, p, err
 				}
 				allJobs[s.Name] = &j
 
+			// case "docker":
+			// 	j, err := NewDockerPipelineJob(serverConfig, s)
+			// 	if err != nil {
+			// 		return allJobs, p, err
+			// 	}
+			// 	allJobs[s.Name] = &j
+
+			// case "local":
+			// 	j, err := NewLocalPipelineJob(serverConfig, s)
+			// 	if err != nil {
+			// 		allJobs[s.Name] = &j
+			// 	}
+
 			default:
-				return p, errors.New(fmt.Sprintf("Unknown platform '%s'", serverConfig.Platform.Name))
+				return allJobs, p, errors.New(fmt.Sprintf("Unknown platform '%s'", serverConfig.Platform.Name))
 			}
 			
 			if len(s.DependsOn) == 0 {
@@ -35,6 +45,19 @@ func NewBasicPipeline(serverConfig *config.ServerConfig, pipelineConfig *BasicPi
 				p.RootJobs = append(p.RootJobs, &ptr)
 			}
 		}
+	}
+
+	return allJobs, p, nil
+}
+
+func NewBasicPipeline(serverConfig *config.ServerConfig, pipelineConfig *BasicPipeline) (*Pipeline, error) {
+	// TODO: validate that integration configurations can be found for the user who submitted this pipeline
+	// if not, return an error to the caller
+
+	
+	allJobs, p, err := createJobsMap(serverConfig, pipelineConfig)
+	if err != nil {
+		return p, err
 	}
 
 	for _, s := range pipelineConfig.Steps {
--- pipelines/local.go
+++ pipelines/local.go
@@ -1,84 +1,96 @@
 package pipelines
 
-// import (
-// 	"fmt"
-// 	"log"
-// 	"sync"
-// 	"os/exec"
-// 	"leveler/config"
-// 	uuid "github.com/satori/go.uuid"
-// )
+import (
+	"fmt"
+	"log"
+	"sync"
+	"os/exec"
+	"leveler/config"
+	uuid "github.com/satori/go.uuid"
+)
 
-// type LocalPipelineJob struct {
-// 	Id string
-// 	Name string
-// 	Workdir string
-// 	Script string
-// 	Env map[string]string
-// 	Inputs []*PipelineInput  // for local jobs, create a socket
-// 	Outputs []*PipelineOutput  // for local jobs, read from a socket
-// 	Parents []*PipelineJob
-// 	Children []*PipelineJob
-// 	Color string  // for detecting cycles in the job graph -- not intended to be used within a job
-// }
+type LocalPipelineJob struct {
+	Id string
+	Name string
+	Workdir string
+	Script string
+	Env map[string]string
+	Inputs []*PipelineInput  // for local jobs, read from a socket
+	Outputs []*PipelineOutput  // for local jobs, write to a socket
+	Parents []*PipelineJob
+	Children []*PipelineJob
+	Color string  // for detecting cycles in the job graph -- not intended to be used within a job
+}
 
-// func NewLocalPipelineJob(serverConfig *config.ServerConfig, jobConfig *PipelineStep) (LocalPipelineJob, error) {
-// 	k := LocalPipelineJob{
-// 		Id: uuid.NewV4().String(),
+func NewLocalPipelineJob(serverConfig *config.ServerConfig, jobConfig *PipelineStep) (LocalPipelineJob, error) {
+	// create workdir under /var/lib/leveler/pipelines/<pipeline-id>/<job-name>
+	// resolve inputs -- i.e., socket locations /var/lib/leveler/pipelines/<pipeline-id>/<dependency-name>/outputs/<output-name>.sock -> /var/lib/leveler/pipelines/<pipeline-id>/<job-name>/inputs/<input-name>.sock
+	// create sockets for outputs in workdir -- /var/lib/leveler/pipelines/<pipeline-id>/<job-name>/outputs/<output-name>.sock
+	// generate script
+	script := fmt.Sprintf(`
+#!/bin/bash
 
-// 		Color: "white",   // for cycle detection -- not meant to be used within a job
-// 	}
+cd %s 
 
-// 	return k, nil
-// }
+`, foo)
 
-// func (j *LocalPipelineJob) SetColor(color string) {
-// 	j.Color = color
-// }
+	k := LocalPipelineJob{
+		Id: uuid.NewV4().String(),
+		Name: jobConfig.Name,
+		Script: script,
+		Color: "white",   // for cycle detection -- not meant to be used within a job
+	}
 
-// func (j *LocalPipelineJob) GetColor() string {
-// 	return j.Color
-// }
+	return k, nil
+}
+
+func (j *LocalPipelineJob) SetColor(color string) {
+	j.Color = color
+}
+
+func (j *LocalPipelineJob) GetColor() string {
+	return j.Color
+}
  
-// func (j *LocalPipelineJob) GetId() string {
-// 	return j.Id
-// }
+func (j *LocalPipelineJob) GetId() string {
+	return j.Id
+}
 
-// func (j *LocalPipelineJob) GetName() string {
-// 	return j.Name
-// }
+func (j *LocalPipelineJob) GetName() string {
+	return j.Name
+}
 
-// func (j *LocalPipelineJob) GetChildren() []*PipelineJob {
-// 	return j.Children
-// }
+func (j *LocalPipelineJob) GetChildren() []*PipelineJob {
+	return j.Children
+}
 
-// func (j *LocalPipelineJob) GetParents() []*PipelineJob {
-// 	return j.Parents
-// }
+func (j *LocalPipelineJob) GetParents() []*PipelineJob {
+	return j.Parents
+}
 
-// func (j *LocalPipelineJob) AddChild(child *PipelineJob) {
-// 	j.Children = append(j.Children, child)
-// }
+func (j *LocalPipelineJob) AddChild(child *PipelineJob) {
+	j.Children = append(j.Children, child)
+}
 
-// func (j *LocalPipelineJob) AddParent(parent *PipelineJob) {
-// 	j.Parents = append(j.Parents, parent)
-// }
+func (j *LocalPipelineJob) AddParent(parent *PipelineJob) {
+	j.Parents = append(j.Parents, parent)
+}
 
-// func (j *LocalPipelineJob) Watch(statuses chan *PipelineJobStatus, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	// watch for the job to complete
-// 	// put status on the channel
-// 	_ = j.Cleanup()
-// }
+func (j *LocalPipelineJob) Watch(statuses chan *PipelineJobStatus, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// watch for the job to complete
+	// put status on the channel
+	_ = j.Cleanup()
+}
 
-// func (j *LocalPipelineJob) Run() error { 
-// 	// create job script
-// 	// resolve inputs 
-// 	// run script, generate outputs
-// 	return nil
-// }
+func (j *LocalPipelineJob) Run() error { 
+	// create job script
+	// resolve inputs 
+	// run script, generate outputs
+	return nil
+}
 
-// func (j *LocalPipelineJob) Cleanup() error {
-// 	// delete working directory
-// 	return nil
-// }
+func (j *LocalPipelineJob) Cleanup() error {
+	// delete working directory
+	return nil
+}
